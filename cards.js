c954ab1266204273770213e7cb2bf027343876c7
Jessica Gilligan <jessicagilligan@lawn-143-215-59-10.lawn.gatech.edu>
2017-04-11 21:23:29 -0400

    added comments to cards.js

--- cards.js
+++ cards.js
@@ -3,14 +3,15 @@ var fs = require("fs");
 var questionInput = "George Washington was the first president of the United States?"
 var answerInput = "George Washington"
 
-
+//create base card to use to prototype
 var Card = function(question, answer) { 
     this.question = question;
     this.answer = answer;
 };
-
+//export card to be used by others files
 module.exports = Card;
 
+//create new card to test constructor & logs to ensure properties log correctly
 var card = new Card (questionInput, answerInput);
 console.log("\n--------card----------");
 console.log("question: ", card.question);
